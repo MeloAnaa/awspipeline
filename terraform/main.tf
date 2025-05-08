d32fa41db3f4cac25cd7a1851786687ca65a96e6
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-08 09:19:55 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "bucket_terraform" {
-  bucket = "meu-bucket-unico-terraformlabestagio"
+  bucket = "meu-bucket-unico-terraformlabestagio1"
 
   tags = {
     Owner      = "ana"
