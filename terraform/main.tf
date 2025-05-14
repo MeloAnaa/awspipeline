3dad3df7e3cd1b24581315430d39a1c89dacf79e
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-14 09:49:31 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,13 @@
 resource "aws_s3_bucket" "meu_bucket" {
-  "acl": "private",
-  "bucket": "testandobuckets3ananana",
-  "force_destroy": false,
-  "tags": {
-    "Environment": "Dev",
-    "Owner": "ana",
-    "Project": "project"
-  },
-  "tags_all": {
-    "Environment": "Dev",
-    "Owner": "ana",
-    "Project": "project"
-  },
-  "timeouts": null
+  acl    = "private"
+  bucket = "testandobuckets3ananana"
+  force_destroy = false
+
+  tags = {
+    Environment = "Dev"
+    Owner       = "ana"
+    Project     = "project"
+  }
 }
 
 
