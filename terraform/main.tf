ee257e05fd83c4751d36e1c1d664c3333db3ec2a
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-14 14:05:45 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,13 +6,13 @@ locals {
   }
 }
 
-# S3 Bucket para armazenar o Cost and Usage Report (CUR)
+# S3 para CUR
 resource "aws_s3_bucket" "cur_bucket" {
   bucket = "finops-cur-bucket-demo"
-  tags   = local.common_tags
+
+  tags = local.common_tags
 }
 
-# Habilita versionamento no bucket
 resource "aws_s3_bucket_versioning" "cur_versioning" {
   bucket = aws_s3_bucket.cur_bucket.id
   versioning_configuration {
@@ -20,37 +20,36 @@ resource "aws_s3_bucket_versioning" "cur_versioning" {
   }
 }
 
-# Bloqueia acesso público ao bucket
 resource "aws_s3_bucket_public_access_block" "cur_block" {
-  bucket                  = aws_s3_bucket.cur_bucket.id
+  bucket = aws_s3_bucket.cur_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-# Cost and Usage Report (CUR)
+# CUR (Cost and Usage Report)
 resource "aws_cur_report_definition" "cur" {
-  report_name                = "finops-cur-report"
-  time_unit                  = "DAILY"
-  format                     = "Parquet"
-  compression                = "Parquet"
+  report_name         = "finops-cur-report"
+  time_unit           = "DAILY"
+  format              = "Parquet"
+  compression         = "Parquet"
   additional_schema_elements = ["RESOURCES"]
-  s3_bucket                  = aws_s3_bucket.cur_bucket.id
-  s3_prefix                  = "cur-data/"
-  s3_region                  = "us-east-1"
-  report_versioning          = "CREATE_NEW_REPORT"
+  s3_bucket           = aws_s3_bucket.cur_bucket.id
+  s3_prefix           = "cur-data/"
+  s3_region           = "us-east-1"
+  report_versioning   = "CREATE_NEW_REPORT"
 
   depends_on = [aws_s3_bucket_public_access_block.cur_block]
 }
 
-# Glue Database para consultar os dados do CUR
+# Glue Database
 resource "aws_glue_catalog_database" "cur_db" {
   name = "finops_cur_db"
   tags = local.common_tags
 }
 
-# Glue Table que referencia os dados do CUR no S3
+# Glue Table (referência ao schema CUR em Parquet)
 resource "aws_glue_catalog_table" "cur_table" {
   name          = "cur_table"
   database_name = aws_glue_catalog_database.cur_db.name
@@ -61,7 +60,7 @@ resource "aws_glue_catalog_table" "cur_table" {
     input_format  = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat"
     output_format = "org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat"
 
-    serde_info {
+    ser_de_info {
       serialization_library = "org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe"
     }
 
@@ -70,13 +69,12 @@ resource "aws_glue_catalog_table" "cur_table" {
       type = "string"
     }
 
-    # Adicione outras colunas conforme o esquema do CUR
+    # Adicione outras colunas aqui conforme a estrutura do CUR
   }
 
   parameters = {
     classification = "parquet"
   }
 
-  tags = local.common_tags
+  # tags não suportado neste recurso, removido
 }
-
