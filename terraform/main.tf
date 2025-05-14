f65d8fd82a2b20e93325009b43114488d566283f
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-14 13:54:49 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-
 locals {
   common_tags = {
     Environment = "labestagioana"
@@ -11,13 +6,13 @@ locals {
   }
 }
 
-#  S3 para CUR
+# S3 Bucket para armazenar o Cost and Usage Report (CUR)
 resource "aws_s3_bucket" "cur_bucket" {
   bucket = "finops-cur-bucket-demo"
-
-  tags = local.common_tags
+  tags   = local.common_tags
 }
 
+# Habilita versionamento no bucket
 resource "aws_s3_bucket_versioning" "cur_versioning" {
   bucket = aws_s3_bucket.cur_bucket.id
   versioning_configuration {
@@ -25,41 +20,41 @@ resource "aws_s3_bucket_versioning" "cur_versioning" {
   }
 }
 
+# Bloqueia acesso público ao bucket
 resource "aws_s3_bucket_public_access_block" "cur_block" {
-  bucket = aws_s3_bucket.cur_bucket.id
+  bucket                  = aws_s3_bucket.cur_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
 
-# CUr
+# Cost and Usage Report (CUR)
 resource "aws_cur_report_definition" "cur" {
-  report_name         = "finops-cur-report"
-  time_unit           = "DAILY"
-  format              = "Parquet"
-  compression         = "Parquet"
+  report_name                = "finops-cur-report"
+  time_unit                  = "DAILY"
+  format                     = "Parquet"
+  compression                = "Parquet"
   additional_schema_elements = ["RESOURCES"]
-  s3_bucket           = aws_s3_bucket.cur_bucket.id
-  s3_prefix           = "cur-data/"
-  s3_region           = "us-east-1"
-  report_versioning   = "CREATE_NEW_REPORT"
+  s3_bucket                  = aws_s3_bucket.cur_bucket.id
+  s3_prefix                  = "cur-data/"
+  s3_region                  = "us-east-1"
+  report_versioning          = "CREATE_NEW_REPORT"
 
   depends_on = [aws_s3_bucket_public_access_block.cur_block]
 }
 
-# Glue database
+# Glue Database para consultar os dados do CUR
 resource "aws_glue_catalog_database" "cur_db" {
   name = "finops_cur_db"
   tags = local.common_tags
 }
 
-# Glue 
+# Glue Table que referencia os dados do CUR no S3
 resource "aws_glue_catalog_table" "cur_table" {
   name          = "cur_table"
   database_name = aws_glue_catalog_database.cur_db.name
-
-  table_type = "EXTERNAL_TABLE"
+  table_type    = "EXTERNAL_TABLE"
 
   storage_descriptor {
     location      = "s3://${aws_s3_bucket.cur_bucket.bucket}/cur-data/"
@@ -75,7 +70,7 @@ resource "aws_glue_catalog_table" "cur_table" {
       type = "string"
     }
 
-    # outras colunas conforme o CUR
+    # Adicione outras colunas conforme o esquema do CUR
   }
 
   parameters = {
@@ -84,3 +79,4 @@ resource "aws_glue_catalog_table" "cur_table" {
 
   tags = local.common_tags
 }
+
