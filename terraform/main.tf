b4dbad4aae8e938506a910bb503c6b69267492b7
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-16 17:15:32 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,6 +38,14 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
 
   egress {
     from_port   = 0
