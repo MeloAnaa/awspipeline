c541a6d306facd090156b904b00f601cc9d46ec3
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-16 16:35:15 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -76,4 +76,16 @@ resource "aws_lb_target_group_attachment" "web_attachment" {
 }
 
 
+resource "aws_s3_bucket" "anaestagiolab2025ana" {
+  bucket = "anaestagiolab2025ana"
+  tags = {
+    Environment = "dev"
+    Name        = "anaestagiolab2025ana"
+    Owner       = "ana"
+    Project     = "Project"
+  }
+}
+
+
+
   
