5ea6d38b190a3fdc48100f28a0bd5ad14a90dda6
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-16 22:29:43 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,8 @@ resource "aws_security_group" "web_sg" {
   }
 
   egress {
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
     self        = false
@@ -37,6 +39,7 @@ resource "aws_security_group" "web_sg" {
     ignore_changes = [revoke_rules_on_delete, timeouts]
   }
 }
+
 
 
 
