2e355188cbf0c4ef6ae72d8e48ecbfce3d066ca5
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-19 11:13:16 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -89,16 +89,6 @@ resource "aws_s3_bucket" "anaestagiolab2025ana" {
 
 
 
-resource "aws_s3_bucket" "anaestagiolab2025ana" {
-  bucket = "anaestagiolab2025ana"
-  tags = {
-    Environment = "dev"
-    Name        = "anaestagiolab2025ana"
-    Owner       = "ana"
-    Project     = "Project"
-  }
-}
-
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.anaestagiolab2025ana.id
   versioning_configuration {
