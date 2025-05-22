4d0f8d899013abe3384bcb9b745b072cc92c06cd
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-22 10:55:17 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,6 +71,7 @@ resource "aws_lb_listener" "web_listener" {
   tags = {
     Name        = "web-listener"
     Environment = "firefly" 
+  }
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 80
   protocol          = "HTTP"
@@ -78,7 +79,6 @@ resource "aws_lb_listener" "web_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web_tg.arn
-  }
   }
 }
 
