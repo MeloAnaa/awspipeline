66bf5e258fc46f07e66097650fcc071352e340cb
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-22 10:50:28 -0300

    atual

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,21 +44,33 @@ resource "aws_security_group" "web_sg" {
 
 
 resource "aws_lb" "web_alb" {
+
+  tags = {
+    Name        = "web-load-balancer"
+    Environment = "firefly" 
   name               = "web-alb"
   internal           = false
   load_balancer_type = "application"
   subnets            = ["subnet-0daf079f949e01bff", "subnet-090eb77b6e57cfe74"]
   security_groups    = [aws_security_group.web_sg.id]  # Corrigido para usar web_sg
+  }
 }
 
 resource "aws_lb_target_group" "web_tg" {
+  tags = {
+    Name        = "web-target-group"
+    Environment = "firefly" 
   name     = "web-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = "vpc-0ac9b054c0e7ec98b"
+  }
 }
 
 resource "aws_lb_listener" "web_listener" {
+  tags = {
+    Name        = "web-listener"
+    Environment = "firefly" 
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 80
   protocol          = "HTTP"
@@ -66,6 +78,7 @@ resource "aws_lb_listener" "web_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web_tg.arn
+  }
   }
 }
 
