a2d773e7ab222869ce45979e44916cf9a706a811
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-05-29 11:02:19 -0300

    atual

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web_tokyo" {
   provider      = aws.tokyo
   ami           = "ami-0c1638aa346a43fe8" 
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
 
   tags = {
-    Name        = "web-instance2"
+    Name        = "web-instancetokyo"
     Owner       = "ana"
     Environment = "teste"
   }
