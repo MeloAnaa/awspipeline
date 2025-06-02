ca270d20d46bc2edadfd547b4c47c71a96cf14b4
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-06-02 13:06:14 -0300

    atualaç

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "tokyo" {
   provider                    = aws.tokyo
   ami                         = "ami-0c1638aa346a43fe8"
   instance_type               = "t2.nano"
-  subnet_id                   = "subnet-0daf079f949e01bff"
+  subnet_id                   = "subnet-01a2eb5d053121039"
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.web_sg.id]   
   tags = {
