dea5280a5916c6e900c84b055dcf0a8fbe9e7bc7
Ana Clara Melo <meloanaclaa299@gmail.com>
2025-06-02 11:42:51 -0300

    atual

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,42 +1,41 @@
-
 resource "aws_instance" "tokyo" {
   ami                         = "ami-0c1638aa346a43fe8"
-  associate_public_ip_address = true
+  instance_type               = "t2.nano"
   availability_zone           = "ap-northeast-1a"
+  subnet_id                   = "subnet-0daf079f949e01bff"
+  security_groups             = ["default"]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_type  = "gp3"
+    volume_size  = 8
+    iops         = 3000
+    throughput   = 125
+    delete_on_termination = true
+  }
+
+  metadata_options {
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 2
+  }
+
   credit_specification {
     cpu_credits = "standard"
   }
-  disable_api_termination = false
-  ebs_optimized           = false
+
   enclave_options {
     enabled = false
   }
-  hibernation                          = false
+
   instance_initiated_shutdown_behavior = "stop"
-  instance_type                        = "t2.nano"
-  metadata_options {
-    http_put_response_hop_limit = 2
-    http_tokens                 = "required"
-  }
-  monitoring = false
-  private_ip = "172.31.40.65"
-  root_block_device {
-    iops        = 3000
-    throughput  = 125
-    volume_size = 8
-    volume_type = "gp3"
-  }
-  security_groups = ["default"]
-  subnet_id       = "subnet-0daf079f949e01bff"
+
   tags = {
     Name        = "instancetokyo"
     Owner       = "ana"
     Environment = "teste"
   }
-  tenancy = "default"
-  # The following attributes have default values introduced when importing the resource into terraform: [timeouts]
+
   lifecycle {
     ignore_changes = [timeouts]
   }
 }
-
